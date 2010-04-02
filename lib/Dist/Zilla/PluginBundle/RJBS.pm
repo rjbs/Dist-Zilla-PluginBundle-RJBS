c6125883fea3774725c6c14e70ed0c65624999d0
Ricardo Signes <rjbs@cpan.org>
2010-04-02 19:14:51 -0400

    use TestRelease

--- lib/Dist/Zilla/PluginBundle/RJBS.pm
+++ lib/Dist/Zilla/PluginBundle/RJBS.pm
@@ -3,12 +3,14 @@ package Dist::Zilla::PluginBundle::RJBS;
 
 use Moose;
 use Moose::Autobox;
-use Dist::Zilla 2;
+use Dist::Zilla 2.100922; # TestRelease
 with 'Dist::Zilla::Role::PluginBundle';
 
 =head1 DESCRIPTION
 
 This is the plugin bundle that RJBS uses.  It is equivalent to:
+
+  [TestRelease]
 
   [@Filter]
   bundle = @Classic
@@ -23,6 +25,9 @@ This is the plugin bundle that RJBS uses.  It is equivalent to:
   [PodWeaver]
   [Repository]
 
+  [@Git]
+  tag_format = %v
+
 If the C<task> argument is given to the bundle, PodWeaver is replaced with
 TaskWeaver.  If the C<manual_version> argument is given, AutoVersion is
 omitted.
@@ -36,10 +41,17 @@ sub bundle_config {
   my ($self, $section) = @_;
   my $class = (ref $self) || $self;
 
-  my $arg = $section->{payload};
+  my $arg     = $section->{payload};
   my $is_task = $arg->{task};
+  my $prefix  = 'Dist::Zilla::Plugin::';
 
-  my @plugins = Dist::Zilla::PluginBundle::Filter->bundle_config({
+  my @plugins;
+
+  push @plugins, [
+    "$section->{name}/TestRelease" => "${prefix}TestRelease" => {}
+  ];
+
+  push @plugins, Dist::Zilla::PluginBundle::Filter->bundle_config({
     name    => $section->{name} . '/@Classic',
     payload => {
       bundle => '@Classic',
@@ -61,7 +73,6 @@ sub bundle_config {
                     . ($ENV{DEV} ? (sprintf '_%03u', $ENV{DEV}) : '') ;
   }
 
-  my $prefix = 'Dist::Zilla::Plugin::';
   my @extra = map {[ "$section->{name}/$_->[0]" => "$prefix$_->[0]" => $_->[1] ]}
   (
     [ AutoPrereq  => {} ],
