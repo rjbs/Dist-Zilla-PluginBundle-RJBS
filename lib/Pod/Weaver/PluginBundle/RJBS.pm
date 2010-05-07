2847b40ea8234bce0665508a079094d2df227558
Ricardo Signes <rjbs@cpan.org>
2010-05-07 19:17:53 -0400

    expand @Default for easier mucking-with

--- lib/Pod/Weaver/PluginBundle/RJBS.pm
+++ lib/Pod/Weaver/PluginBundle/RJBS.pm
@@ -5,7 +5,7 @@ package Pod::Weaver::PluginBundle::RJBS;
 
 =head1 OVERVIEW
 
-Equivalent to:
+Roughly equivalent to:
 
 =for :list
 * C<@Default>
@@ -13,13 +13,40 @@ Equivalent to:
 
 =cut
 
+use Pod::Weaver::Config::Assembler;
+sub _exp { Pod::Weaver::Config::Assembler->expand_package($_[0]) }
+
 sub mvp_bundle_config {
-  return (
-    [ '@RJBS/Default', 'Pod::Weaver::PluginBundle::Default', {} ],
-    [ '@RJBS/List',    'Pod::Weaver::Plugin::Transformer',
-      { 'transformer' => 'List' }
-    ],
+  my @plugins;
+  push @plugins, (
+    [ '@RJBS/CorePrep',    _exp('@CorePrep'), {} ],
+    [ '@RJBS/Name',        _exp('Name'),      {} ],
+    [ '@RJBS/Version',     _exp('Version'),   {} ],
+
+    [ '@RJBS/Prelude',     _exp('Region'),  { region_name => 'prelude'     } ],
+    [ '@RJBS/Synopsis',    _exp('Generic'), { header      => 'SYNOPSIS'    } ],
+    [ '@RJBS/Description', _exp('Generic'), { header      => 'DESCRIPTION' } ],
+    [ '@RJBS/Overview',    _exp('Generic'), { header      => 'OVERVIEW'    } ],
   );
+
+  for my $plugin (
+    [ 'Attributes', _exp('Collect'), { command => 'attr'   } ],
+    [ 'Methods',    _exp('Collect'), { command => 'method' } ],
+    [ 'Functions',  _exp('Collect'), { command => 'func'   } ],
+  ) {
+    $plugin->[2]{header} = uc $plugin->[0];
+    push @plugins, $plugin;
+  }
+
+  push @plugins, (
+    [ '@RJBS/Leftovers', _exp('Leftovers'), {} ],
+    [ '@RJBS/postlude',  _exp('Region'),    { region_name => 'postlude' } ],
+    [ '@RJBS/Authors',   _exp('Authors'),   {} ],
+    [ '@RJBS/Legal',     _exp('Legal'),     {} ],
+    [ '@RJBS/List',      _exp('-Transformer'), { 'transformer' => 'List' } ],
+  );
+
+  return @plugins;
 }
 
 1;
