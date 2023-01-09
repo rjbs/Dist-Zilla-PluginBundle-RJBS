6039a3cf4f71f6f3b741a55885efd2eb7f4f19c0
Ricardo Signes <rjbs@semiotic.systems>
2023-01-08 20:54:06 -0500

    workflower: draw the rest of the owl

--- lib/Dist/Zilla/App/Command/workflower.pm
+++ lib/Dist/Zilla/App/Command/workflower.pm
@@ -5,6 +5,12 @@ use Dist::Zilla::Pragmas;
 
 use Dist::Zilla::App -command;
 
+use Sub::Exporter::ForMethods ();
+use Data::Section
+  { installer => Sub::Exporter::ForMethods::method_installer },
+  -setup => {};
+use Path::Tiny;
+
 =head1 SYNOPSIS
 
   dzil workflower
@@ -28,6 +34,22 @@ sub abstract { "install rjbs's usual GitHub Actions workflow" }
 sub execute {
   my ($self, $opt, $arg) = @_;
 
+  my $template = $self->section_data('workflow.yml')->$*;
+  my $versions = sprintf '[ %s ]',
+                  join q{, }, map {; qq{"$_"} } $self->_perl_versions_to_test;
+
+  $template =~ s/%%VERSIONS%%/$versions/g;
+
+  my $workflow_dir = path(".github/workflows");
+  $workflow_dir->mkpath;
+
+  $workflow_dir->child('multiperl-test.yml')->spew_utf8($template);
+
+  $self->zilla->log("Workflow installed.");
+  return;
+}
+
+sub _perl_versions_to_test ($self) {
   my $zilla = $self->zilla;
 
   $_->before_build       for $zilla->plugins_with(-BeforeBuild)->@*;
@@ -38,12 +60,104 @@ sub execute {
   $_->register_prereqs   for $zilla->plugins_with(-PrereqSource)->@*;
 
   my $prereqs = $zilla->prereqs;
+  $prereqs->finalize;
 
-  my $minimum_perl = $prereqs->merged_prereqs;
+  my $merged = $prereqs->merged_requires;
 
-  for (my $i = 8; $i <= 36; $i += 2) {
-    say $i;
+  my @test = qw(latest);
+
+  for (my $i = 36; $i >= 8; $i -= 2) {
+    last unless $merged->accepts_module(perl => "v5.$i");
+    push @test, "5.$i";
   }
+
+  return @test;
 }
 
 1;
+__DATA__
+___[ workflow.yml ]___
+name: "multiperl test"
+on: [ push, pull_request ]
+
+# FUTURE ENHANCEMENT(s):
+# * install faster (see below)
+# * use github.event.repository.name or ${GITHUB_REPOSITORY#*/} as the
+#   tarball/build name instead of Dist-To-Test
+
+jobs:
+  build-tarball:
+    runs-on: ubuntu-latest
+    strategy:
+      fail-fast: false
+    steps:
+      - name: Check out repo
+        uses: actions/checkout@v3
+      - name: Install cpanminus
+        run: |
+          curl https://cpanmin.us/ > /tmp/cpanm
+          chmod u+x /tmp/cpanm
+      - name: Install Dist::Zilla
+        run: sudo apt-get install -y libdist-zilla-perl
+      - name: Install prereqs
+        # This could probably be made more efficient by looking at what it's
+        # installing via cpanm that could, instead, be installed from apt.  I
+        # may do that later, but for now, it's fine! -- rjbs, 2023-01-07
+        run: |
+          dzil authordeps --missing | /tmp/cpanm --notest -S
+          dzil listdeps --author --missing | /tmp/cpanm --notest -S
+      - name: Build tarball
+        run: |
+          dzil build --in Dist-To-Test
+          tar zcvf Dist-To-Test.tar.gz Dist-To-Test
+      - name: Upload tarball
+        uses: actions/upload-artifact@v3
+        with:
+          name: Dist-To-Test.tar.gz
+          path: Dist-To-Test.tar.gz
+
+  multiperl-test:
+    needs: build-tarball
+    env:
+      # some plugins still needs this to run their tests...
+      PERL_USE_UNSAFE_INC: 0
+      AUTHOR_TESTING: 1
+      AUTOMATED_TESTING: 1
+
+    runs-on: ubuntu-latest
+
+    strategy:
+      fail-fast: false
+      matrix:
+        perl-version: %%VERSIONS%%
+
+    container:
+      image: perldocker/perl-tester:${{ matrix.perl-version }}
+
+    steps:
+      - name: Download tarball
+        uses: actions/download-artifact@v3
+        with:
+          name: Dist-To-Test.tar.gz
+      - name: Extract tarball
+        run: tar zxvf Dist-To-Test.tar.gz
+      - name: Install dependencies
+        working-directory: ./Dist-To-Test
+        run: cpanm --installdeps --notest .
+      - name: Makefile.PL
+        working-directory: ./Dist-To-Test
+        run: perl Makefile.PL
+      - name: Install yath
+        run: cpanm --notest Test2::Harness
+      - name: Install testing libraries
+        run: cpanm --notest Test2::Harness::Renderer::JUnit
+      - name: Run the tests
+        working-directory: ./Dist-To-Test
+        run: |
+          JUNIT_TEST_FILE="/tmp/test-output.xml" ALLOW_PASSING_TODOS=1 yath test --renderer=Formatter --renderer=JUnit -D
+      - name: Publish test report
+        uses: mikepenz/action-junit-report@v3
+        if: always() # always run even if the previous step fails
+        with:
+          check_name: JUnit Report (${{ matrix.perl-version }})
+          report_paths: /tmp/test-output.xml
